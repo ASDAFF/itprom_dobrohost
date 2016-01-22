0ccf49350978dbb54bef9cb53710682f0e69b027
Hasan <hasan.akniyev@gmail.com>
2016-01-22 23:52:31 +0300

    fix

---
+++ www/bitrix/templates/webfly_san/js/gallery-cert.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var options = {
+        auto: true,
+        visible: 1,
+        circular: true,
+        speed: 1000,
+        pause: true,
+        btnGo: $('div.nav a'),
+        btnNext: '.next',
+        btnPrev: '.prev',
+        activeClass: 'active'
+    };
+
+
+    $('.slideshow').jCarouselLite(options);
+});
