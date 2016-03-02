d856ebb223afd177ae9fd9a8cfc13b31ffeecf53
artshar <sharapudinov@mial.ru>
2016-03-02 19:58:02 +0300

    index.php redirectё

--- www/bitrix/templates/webfly_san/components/bitrix/catalog/santech1_5/bitrix/catalog.section.list/section/template.php
+++ www/bitrix/templates/webfly_san/components/bitrix/catalog/santech1_5/bitrix/catalog.section.list/section/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -10,26 +10,31 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
+
+global $APPLICATION;
+
+$curDir = $APPLICATION->GetCurDir();
+
 $_SESSION["mywf"]["el_cnt"] = $arResult['SECTION']["ELEMENT_CNT"];
 $this->setFrameMode(true);
 $arViewModeList = $arResult['VIEW_MODE_LIST'];
 $arViewStyles = array(
-	'TILE' => array(
-		'CONT' => 'bx_catalog_tile',
-		'TITLE' => 'bx_catalog_tile_category_title',
-		'LIST' => 'bx_catalog_tile_ul',
-		'EMPTY_IMG' => $this->GetFolder().'/images/tile-empty.png'
-	)
+    'TILE' => array(
+        'CONT' => 'bx_catalog_tile',
+        'TITLE' => 'bx_catalog_tile_category_title',
+        'LIST' => 'bx_catalog_tile_ul',
+        'EMPTY_IMG' => $this->GetFolder() . '/images/tile-empty.png'
+    )
 );
 $arCurView = $arViewStyles[$arParams['VIEW_MODE']];
 
 $strSectionEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_EDIT");
 $strSectionDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_DELETE");
 $arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_CONFIRM'));
-$shortText = explode("*MORE*",$arResult["SECTION"]["~DESCRIPTION"]);
+$shortText = explode("*MORE*", $arResult["SECTION"]["~DESCRIPTION"]);
 ?>
 
-<?/*
+<? /*
 <div class="content-text">
   <h1><?=isset($arResult['SECTION']["IPROPERTY_VALUES"]["SECTION_PAGE_TITLE"]) && $arResult['SECTION']["IPROPERTY_VALUES"]["SECTION_PAGE_TITLE"] != ""
 			? $arResult['SECTION']["IPROPERTY_VALUES"]["SECTION_PAGE_TITLE"]
@@ -42,72 +47,86 @@ $shortText = explode("*MORE*",$arResult["SECTION"]["~DESCRIPTION"]);
   <a href="javascript:void(0);" class="more"><span class="text1">Больше</span><span class="text2">Меньше</span></a>
   <?endif;?>
 </div>
-*/?>
+*/ ?>
 
-<style>
-	.pheader2 {
-		text-align: left;
-		font-weight: bold;
-		display: block;
-		font-size: 1.5em;
-		margin: 0 0 0 10px;
-		-webkit-margin-before: 0.83em;
-		-webkit-margin-after: 0.83em;
-		-webkit-margin-end: 0px;
-	}
-</style>
+    <style>
+        .pheader2 {
+            text-align: left;
+            font-weight: bold;
+            display: block;
+            font-size: 1.5em;
+            margin: 0 0 0 10px;
+            -webkit-margin-before: 0.83em;
+            -webkit-margin-after: 0.83em;
+            -webkit-margin-end: 0px;
+        }
+    </style>
 
 
 <?
-if (0 < $arResult["SECTIONS_COUNT"]){?>
-<div class="product-catalog product-catalog03 myProdCat section">
-<ul class="<?= $arCurView['LIST']; ?>">
-<?
-	switch ($arParams['VIEW_MODE']){
-    case 'TILE':default:
-			foreach ($arResult['SECTIONS'] as &$arSection){
-				$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-				$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
+if (0 < $arResult["SECTIONS_COUNT"]) {
+    ?>
+    <div class="product-catalog product-catalog03 myProdCat section">
+        <ul class="<?= $arCurView['LIST']; ?>">
+            <?
+            switch ($arParams['VIEW_MODE']) {
+                case 'TILE':
+                default:
+                    foreach ($arResult['SECTIONS'] as &$arSection) {
+                        $isSelected = str_replace($arSection['CODE'], '', $curDir) != $curDir;
+                        $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
+                        $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
 
-				if (false === $arSection['PICTURE'])
-					$arSection['PICTURE'] = array(
-						'SRC' => $arCurView['EMPTY_IMG'],
-						'ALT' => (
-							'' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
-							? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
-							: $arSection["NAME"]
-						),
-						'TITLE' => (
-							'' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
-							? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
-							: $arSection["NAME"]
-						)
-					);
-				?>
-        <li id="<?= $this->GetEditAreaId($arSection['ID']); ?>">
-          <div class="hold">
-            <div class="visual">
-              <a href="<?= $arSection['SECTION_PAGE_URL']; ?>" title="<?= $arSection['PICTURE']['TITLE']; ?>">
-                <img width="50" height="40" alt="<?=$arSection['PICTURE']['ALT']?>" src="<?=$arSection['PICTURE']['SRC']?>">
-              </a>
-            </div>
-            <div class="block">
-              <div class="description">
-                <a href="<?= $arSection['SECTION_PAGE_URL']; ?>" title="<?= $arSection['PICTURE']['TITLE']; ?>" class="section-head">
-					<p class="pheader2"><?=$arSection["NAME"]?></p>
-				</a>
-              </div>
-            </div>
-          </div>
-        </li><?
-			}       
-			unset($arSection);
-			break;
-	}
-?>
-</ul>
-</div>
-<?
-	echo ('LINE' != $arParams['VIEW_MODE'] ? '<div style="clear: both;"></div>' : '');
+                        if (false === $arSection['PICTURE'])
+                            $arSection['PICTURE'] = array(
+                                'SRC' => $arCurView['EMPTY_IMG'],
+                                'ALT' => (
+                                '' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
+                                    ? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_ALT"]
+                                    : $arSection["NAME"]
+                                ),
+                                'TITLE' => (
+                                '' != $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
+                                    ? $arSection["IPROPERTY_VALUES"]["SECTION_PICTURE_FILE_TITLE"]
+                                    : $arSection["NAME"]
+                                )
+                            );
+                        ?>
+                    <li <?= $isSelected ? 'class="selected" ' : '' ?>id="<?= $this->GetEditAreaId($arSection['ID']); ?>">
+                        <div class="hold">
+                            <div class="visual">
+                                <? if ($isSelected): ?>
+                                <a href="<?= $arSection['SECTION_PAGE_URL']; ?>"
+                                   title="<?= $arSection['PICTURE']['TITLE']; ?>">
+                                    <?endif ?>
+                                    <img width="50" height="40" alt="<?= $arSection['PICTURE']['ALT'] ?>"
+                                         src="<?= $arSection['PICTURE']['SRC'] ?>">
+                                    <? if ($isSelected): ?>
+                                </a>
+                            <?endif ?>
+                            </div>
+                            <div class="block">
+                                <div class="description">
+                                    <? if ($isSelected): ?>
+                                    <a href="<?= $arSection['SECTION_PAGE_URL']; ?>"
+                                       title="<?= $arSection['PICTURE']['TITLE']; ?>" class="section-head">
+                                        <?endif ?>
+                                        <p class="pheader2"><?= $arSection["NAME"] ?></p>
+                                        <? if ($isSelected): ?>
+                                    </a>
+                                <?endif ?>
+                                </div>
+                            </div>
+                        </div>
+                        </li><?
+                    }
+                    unset($arSection);
+                    break;
+            }
+            ?>
+        </ul>
+    </div>
+    <?
+    echo('LINE' != $arParams['VIEW_MODE'] ? '<div style="clear: both;"></div>' : '');
 }
 ?>
