24bfc26c780652d2af43ef7d74d6402ec5998fa9
artshar <sharapudinov@mial.ru>
2016-03-03 03:44:26 +0300

    index.php redirectё

--- www/index.php
+++ www/index.php
@@ -114,6 +114,61 @@ $APPLICATION->SetTitle("Термошкафы ITProm"); ?>
                                 <a class='next' href="#"> > </a>
                             </div>
                         </div>
+
+                        <div class="block-type02">
+                            <? $APPLICATION->IncludeComponent(
+                                "bitrix:news.list",
+                                "main_news",
+                                Array(
+                                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                    "ADD_SECTIONS_CHAIN" => "Y",
+                                    "AJAX_MODE" => "N",
+                                    "AJAX_OPTION_ADDITIONAL" => "",
+                                    "AJAX_OPTION_HISTORY" => "N",
+                                    "AJAX_OPTION_JUMP" => "N",
+                                    "AJAX_OPTION_STYLE" => "Y",
+                                    "CACHE_FILTER" => "N",
+                                    "CACHE_GROUPS" => "Y",
+                                    "CACHE_TIME" => "36000000",
+                                    "CACHE_TYPE" => "A",
+                                    "CHECK_DATES" => "Y",
+                                    "DETAIL_URL" => "",
+                                    "DISPLAY_BOTTOM_PAGER" => "N",
+                                    "DISPLAY_DATE" => "Y",
+                                    "DISPLAY_NAME" => "Y",
+                                    "DISPLAY_PICTURE" => "Y",
+                                    "DISPLAY_PREVIEW_TEXT" => "Y",
+                                    "DISPLAY_TOP_PAGER" => "N",
+                                    "FIELD_CODE" => array(0 => "", 1 => "",),
+                                    "FILTER_NAME" => "",
+                                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                                    "IBLOCK_ID" => "6",
+                                    "IBLOCK_TYPE" => "news",
+                                    "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                                    "INCLUDE_SUBSECTIONS" => "Y",
+                                    "NEWS_COUNT" => "2",
+                                    "PAGER_DESC_NUMBERING" => "N",
+                                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                    "PAGER_SHOW_ALL" => "N",
+                                    "PAGER_SHOW_ALWAYS" => "N",
+                                    "PAGER_TEMPLATE" => ".default",
+                                    "PAGER_TITLE" => "",
+                                    "PARENT_SECTION" => "",
+                                    "PARENT_SECTION_CODE" => "",
+                                    "PREVIEW_TRUNCATE_LEN" => "",
+                                    "PROPERTY_CODE" => array(0 => "", 1 => "",),
+                                    "SET_BROWSER_TITLE" => "Y",
+                                    "SET_META_DESCRIPTION" => "Y",
+                                    "SET_META_KEYWORDS" => "Y",
+                                    "SET_STATUS_404" => "N",
+                                    "SET_TITLE" => "N",
+                                    "SORT_BY1" => "ACTIVE_FROM",
+                                    "SORT_BY2" => "SORT",
+                                    "SORT_ORDER1" => "DESC",
+                                    "SORT_ORDER2" => "ASC"
+                                )
+                            ); ?>
+                        </div>
                         <? $APPLICATION->IncludeComponent(
                             "bitrix:news.list",
                             "main_blog",
@@ -173,60 +228,6 @@ $APPLICATION->SetTitle("Термошкафы ITProm"); ?>
                         ); ?>
 
 
-                        <div class="block-type02">
-                            <? $APPLICATION->IncludeComponent(
-                                "bitrix:news.list",
-                                "main_news",
-                                Array(
-                                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
-                                    "ADD_SECTIONS_CHAIN" => "Y",
-                                    "AJAX_MODE" => "N",
-                                    "AJAX_OPTION_ADDITIONAL" => "",
-                                    "AJAX_OPTION_HISTORY" => "N",
-                                    "AJAX_OPTION_JUMP" => "N",
-                                    "AJAX_OPTION_STYLE" => "Y",
-                                    "CACHE_FILTER" => "N",
-                                    "CACHE_GROUPS" => "Y",
-                                    "CACHE_TIME" => "36000000",
-                                    "CACHE_TYPE" => "A",
-                                    "CHECK_DATES" => "Y",
-                                    "DETAIL_URL" => "",
-                                    "DISPLAY_BOTTOM_PAGER" => "N",
-                                    "DISPLAY_DATE" => "Y",
-                                    "DISPLAY_NAME" => "Y",
-                                    "DISPLAY_PICTURE" => "Y",
-                                    "DISPLAY_PREVIEW_TEXT" => "Y",
-                                    "DISPLAY_TOP_PAGER" => "N",
-                                    "FIELD_CODE" => array(0 => "", 1 => "",),
-                                    "FILTER_NAME" => "",
-                                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-                                    "IBLOCK_ID" => "6",
-                                    "IBLOCK_TYPE" => "news",
-                                    "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-                                    "INCLUDE_SUBSECTIONS" => "Y",
-                                    "NEWS_COUNT" => "2",
-                                    "PAGER_DESC_NUMBERING" => "N",
-                                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-                                    "PAGER_SHOW_ALL" => "N",
-                                    "PAGER_SHOW_ALWAYS" => "N",
-                                    "PAGER_TEMPLATE" => ".default",
-                                    "PAGER_TITLE" => "",
-                                    "PARENT_SECTION" => "",
-                                    "PARENT_SECTION_CODE" => "",
-                                    "PREVIEW_TRUNCATE_LEN" => "",
-                                    "PROPERTY_CODE" => array(0 => "", 1 => "",),
-                                    "SET_BROWSER_TITLE" => "Y",
-                                    "SET_META_DESCRIPTION" => "Y",
-                                    "SET_META_KEYWORDS" => "Y",
-                                    "SET_STATUS_404" => "N",
-                                    "SET_TITLE" => "N",
-                                    "SORT_BY1" => "ACTIVE_FROM",
-                                    "SORT_BY2" => "SORT",
-                                    "SORT_ORDER1" => "DESC",
-                                    "SORT_ORDER2" => "ASC"
-                                )
-                            ); ?>
-                        </div>
                     </div>
                     <!--   MainContent (tabs) starts here -->
                     <div class="tabset">
